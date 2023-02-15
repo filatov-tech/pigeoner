8d0efef83912b5a0ef9fc24febb3286f03f6f741
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-02-15 10:53:15 -0300

    Init

---
+++ src/main/java/ru/pigeoner/pigeoner/PigeonerApplication.java
@@ -0,0 +1,13 @@
+package ru.pigeoner.pigeoner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/pigeoner/pigeoner/PigeonerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pigeoner.pigeoner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
