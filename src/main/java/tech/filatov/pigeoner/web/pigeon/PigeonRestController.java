15570e7a38b68ec98d2113d6328d08c44187702c
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-03-22 16:42:52 -0300

    Add pigeons filter rest method with @RequestParam

--- src/main/java/tech/filatov/pigeoner/web/pigeon/PigeonRestController.java
+++ src/main/java/tech/filatov/pigeoner/web/pigeon/PigeonRestController.java
@@ -2,12 +2,14 @@ package tech.filatov.pigeoner.web.pigeon;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tech.filatov.pigeoner.dto.PigeonTableDto;
 import tech.filatov.pigeoner.service.PigeonService;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/pigeons")
@@ -22,5 +24,10 @@ public class PigeonRestController {
     @GetMapping
     public List<PigeonTableDto> getAll() {
         return service.getAll(new HashMap<>());
+    }
+
+    @GetMapping("/filter")
+    public List<PigeonTableDto> getFiltered(@RequestParam(required = false) Map<String, String> filterParameters) {
+        return service.getAll(filterParameters);
     }
 }
