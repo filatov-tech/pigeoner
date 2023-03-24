8586f3d4d2780f25fdd54464f3e8315773b806d7
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-03-24 12:23:53 -0300

    Remove unnecessary code from PigeonController

--- src/main/java/tech/filatov/pigeoner/web/pigeon/PigeonController.java
+++ src/main/java/tech/filatov/pigeoner/web/pigeon/PigeonController.java
@@ -1,35 +1,15 @@
 package tech.filatov.pigeoner.web.pigeon;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import tech.filatov.pigeoner.service.PigeonService;
-
-import java.util.Map;
-
-import static tech.filatov.pigeoner.util.ValidationUtil.*;
 
 @Controller
 @RequestMapping("/pigeons")
 public class PigeonController {
 
-    private final PigeonService service;
-
-    public PigeonController(PigeonService service) {
-        this.service = service;
-    }
-
     @GetMapping
-    public String pigeons(@RequestParam(required = false) Map<String, String> filterParameters, ModelMap model) {
-        if (!filterParameters.isEmpty()) {
-            validateDataFromFilter(filterParameters);
-        }
-        model.addAttribute("pigeons", service.getAll(filterParameters));
+    public String pigeons() {
         return "page32658356";
     }
 }
