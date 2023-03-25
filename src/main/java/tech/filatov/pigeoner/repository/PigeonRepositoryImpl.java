505995771309cccb3bbb7ed76e9a1f9db20946ae
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-03-25 19:04:46 -0300

    Add filtering by age and birthdate

--- src/main/java/tech/filatov/pigeoner/repository/PigeonRepositoryImpl.java
+++ src/main/java/tech/filatov/pigeoner/repository/PigeonRepositoryImpl.java
@@ -10,8 +10,10 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import java.util.List;
-import java.util.Map;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.TemporalAmount;
+import java.util.*;
 
 import static tech.filatov.pigeoner.constant.Constants.*;
 
@@ -31,6 +33,14 @@ public class PigeonRepositoryImpl implements PigeonRepositoryCustom {
 
         Root<Pigeon> pigeonRoot = cq.from(Pigeon.class);
         cq.select(pigeonRoot);
+        /*
+         *  TODO: отрефакторить эту простыню
+         *   вариант - создать объект, который будет вначале сконфигурирован билдером имеющимися
+         *   параметрами, такими как CriteriaBuilder, списком фильтров, Root-объектом, списком предикат,
+         *   а затем вернет составленный верно список предикат.
+         *   То есть, вся работа по наполнению запроса динамическими параметрами уйдет в отдельный объект и не будет
+         *   засорять здесь метод
+         */
 
 
         List<Predicate> predicates = new ArrayList<>();
@@ -51,6 +61,71 @@ public class PigeonRepositoryImpl implements PigeonRepositoryCustom {
         if (!filterParameters.get(PIGEON_NAME).isEmpty()) {
             predicates.add(cb.equal(pigeonRoot.get(PIGEON_NAME), filterParameters.get(PIGEON_NAME)));
         }
+
+        String filterDateType = filterParameters.get(FILTER_DATE_TYPE);
+        if (!filterDateType.isEmpty()) {
+            if (filterDateType.equals(BIRTHDATE_TYPE)) {
+                if (!filterParameters.get(BIRTHDATE_FROM).isEmpty()) {
+                    predicates.add(
+                            cb.greaterThan(pigeonRoot.get("birthdate"), LocalDate.parse(filterParameters.get(BIRTHDATE_FROM)))
+                    );
+                }
+                if (!filterParameters.get(BIRTHDATE_TO).isEmpty()) {
+                    predicates.add(
+                            cb.lessThan(pigeonRoot.get("birthdate"), LocalDate.parse(filterParameters.get(BIRTHDATE_TO)))
+                    );
+                }
+            } else if (filterDateType.equals(AGE_TYPE)) {
+                LocalDate now = LocalDate.now();
+
+                LocalDate from = LocalDate.now();
+                if (!filterParameters.get(AGE_YEAR_FROM).isEmpty() || !filterParameters.get(AGE_MONTH_FROM).isEmpty()) {
+                    int yearsFrom = 0;
+                    int monthsFrom = 0;
+                    try {
+                        yearsFrom = Integer.parseInt(filterParameters.get(AGE_YEAR_FROM));
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        monthsFrom = Integer.parseInt(filterParameters.get(AGE_MONTH_FROM));
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                    TemporalAmount yearsMinus = Period.ofYears(yearsFrom);
+                    TemporalAmount monthsMinus = Period.ofMonths(monthsFrom);
+                    from = now.minus(yearsMinus).minus(monthsMinus);
+                }
+
+                LocalDate to = LocalDate.now();
+                if (!filterParameters.get(AGE_YEAR_TO).isEmpty() || !filterParameters.get(AGE_MONTH_TO).isEmpty()) {
+                    int yearsTo = 0;
+                    int monthsTo = 0;
+                    try {
+                        yearsTo = Integer.parseInt(filterParameters.get(AGE_YEAR_TO));
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        monthsTo = Integer.parseInt(filterParameters.get(AGE_MONTH_TO));
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                    TemporalAmount yearsMin = Period.ofYears(yearsTo);
+                    TemporalAmount monthsMin = Period.ofMonths(monthsTo);
+                    to = now.minus(yearsMin).minus(monthsMin);
+                }
+                if (!from.isEqual(now)) {
+                    predicates.add(cb.lessThan(pigeonRoot.get("birthdate"), from));
+                }
+                if (!to.isEqual(now)) {
+                    predicates.add(cb.greaterThan(pigeonRoot.get("birthdate"), to));
+                }
+            }
+        }
+
+        cq.where(predicates.toArray(new Predicate[predicates.size()]));
+
         TypedQuery<Pigeon> executableQuery = em.createQuery(cq);
 
         return executableQuery.getResultList();
