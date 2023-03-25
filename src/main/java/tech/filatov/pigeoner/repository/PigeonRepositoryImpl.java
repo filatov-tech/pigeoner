3b2a85fb86210e6644e6e57cb74b6863e699eb5e
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-03-25 17:14:26 -0300

    Fix dynamic query construction

--- src/main/java/tech/filatov/pigeoner/repository/PigeonRepositoryImpl.java
+++ src/main/java/tech/filatov/pigeoner/repository/PigeonRepositoryImpl.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Map;
@@ -31,19 +32,24 @@ public class PigeonRepositoryImpl implements PigeonRepositoryCustom {
         Root<Pigeon> pigeonRoot = cq.from(Pigeon.class);
         cq.select(pigeonRoot);
 
+
+        List<Predicate> predicates = new ArrayList<>();
         if (!filterParameters.get(RING_NUMBER).isEmpty()) {
-            cq.where(cb.equal(pigeonRoot.get(RING_NUMBER), filterParameters.get(RING_NUMBER)));
+            predicates.add(cb.equal(pigeonRoot.get(RING_NUMBER), filterParameters.get(RING_NUMBER)));
         }
         if (!filterParameters.get(CONDITION).isEmpty()) {
-            cq.where(
-                    cb.equal(pigeonRoot.get("conditionStatus"),
-                    Condition.valueOfLabel(filterParameters.get(CONDITION))));
+            predicates.add(
+                    cb.equal(
+                            pigeonRoot.get("conditionStatus"),
+                            Condition.valueOfLabel(filterParameters.get(CONDITION))
+                    )
+            );
         }
         if (!filterParameters.get(LOCATION).isEmpty()) {
-            cq.where(cb.equal(pigeonRoot.get(LOCATION), filterParameters.get(LOCATION)));
+            predicates.add(cb.equal(pigeonRoot.get(LOCATION), filterParameters.get(LOCATION)));
         }
         if (!filterParameters.get(PIGEON_NAME).isEmpty()) {
-            cq.where(cb.equal(pigeonRoot.get(PIGEON_NAME), filterParameters.get(PIGEON_NAME)));
+            predicates.add(cb.equal(pigeonRoot.get(PIGEON_NAME), filterParameters.get(PIGEON_NAME)));
         }
         TypedQuery<Pigeon> executableQuery = em.createQuery(cq);
 
