8f9817917ffa9afa07f5c6728d00c1c454ef18b3
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-05-19 17:51:30 -0300

    Fix bug

--- src/main/resources/static/components/pigeoner-accordion.js
+++ src/main/resources/static/components/pigeoner-accordion.js
@@ -135,11 +135,11 @@ class PigeonLabel extends React.Component {
         const pigeon = this.props.pigeon;
         const col = this.props.isOutside ? "col-6 col-md-2" : "col-12";
         return (
-            <>
+            <React.Fragment>
                 {pigeon
                     ? <div className={pigeon.male ? `m-2 ${col} label label-male` : `m-2 ${col} label label-female`}>{pigeon.ringNumber}</div>
                     : <div className={`m-2 ${col} label label-vacant`}>Cвободно</div>}
-            </>
+            </React.Fragment>
         );
     }
 }
