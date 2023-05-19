8f4254cba2e566c616e36921767226d27884d7a3
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-05-18 22:38:19 -0300

    Improve nest's view in dovecote section

--- src/main/resources/static/components/pigeoner-accordion.js
+++ src/main/resources/static/components/pigeoner-accordion.js
@@ -61,7 +61,11 @@ class MyAccordion extends React.Component {
         if (error) {
             return <div>Ошибка! {error.message}</div>;
         } else if (!isLoaded) {
-            return <div>Loading...</div>
+            return (
+                <div className="spinner-border text-primary m-5" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            );
         } else {
             const dataBsTarget = `#accordion-${parentId} .item-`;
             const ariaControls = `accordion-${parentId} .item-`;
@@ -78,10 +82,22 @@ class MyAccordion extends React.Component {
                                 </h2>
                                 <div className={"accordion-collapse collapse item-" + section.id} role="tabpanel">
                                     <div className="accordion-body">
-                                        {!(section.sectionType === "NEST") && <strong>Секции:</strong>}
-                                        {section.children && <MyAccordion sections={section.children} parentId={section.id}/>}
+                                        {(section.children.length > 0 && section.children[0].sectionType === "NEST")
+                                            ?
+                                            <div className="row g-2">
+                                                {section.children.map(nest => (
+                                                    <MyCard pigeons={nest.pigeons} name={nest.name}/>
+                                                ))}
+                                            </div>
+                                            :
+                                            (<React.Fragment>
+                                                <strong>Секции:</strong>
+                                                <MyAccordion sections={section.children} parentId={section.id}/>
+                                            </React.Fragment>)}
                                         {section.pigeons.length > 0 &&
                                             <React.Fragment>
+                                                <br/>
+                                                <br/>
                                                 <strong>Голуби:</strong>
                                                 <PigeonList pigeons={section.pigeons} />
                                             </React.Fragment>}
@@ -101,9 +117,44 @@ class PigeonList extends React.Component {
         const pigeons = this.props.pigeons;
         return (<React.Fragment>
             {pigeons && pigeons.map(pigeon => (
-                <p>{pigeon.ringNumber} {pigeon.male ? "M" : "Ж"}</p>
+                <p>{pigeon.ringNumber} {pigeon.isMale ? "M" : "Ж"}</p>
             ))}
         </React.Fragment>)
+    }
+}
+
+class MyCard extends React.Component {
+    render() {
+        const pigeons = this.props.pigeons;
+        const name = this.props.name;
+        return (
+            <div className="col-md-3 nest">
+                <div className="card">
+                    <div className="card-header">
+                        <h5 className="mb-0">{name}</h5>
+                    </div>
+                    <div className="card-body">
+                        <div className="row">
+                            <MyLabel pigeon={pigeons[0]} />
+                            <MyLabel pigeon={pigeons[1]} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+class MyLabel extends React.Component {
+    render() {
+        const pigeon = this.props.pigeon;
+        return (
+            <div className={"col-6 text-center"}>
+                {pigeon
+                    ? <div className={pigeon.male ? "label label-male" : "label label-female"}>{pigeon.ringNumber}</div>
+                    : <div className={"label label-vacant"}>Cвободно</div>}
+            </div>
+        );
     }
 }
 
