da2f3a6d32c2f4e1e10fed6f0b0eb1f3f77bfb87
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-07-02 11:56:38 -0300

    Add new Flight's field to DTO

--- src/main/java/tech/filatov/pigeoner/dto/FlightDto.java
+++ src/main/java/tech/filatov/pigeoner/dto/FlightDto.java
@@ -3,10 +3,10 @@ package tech.filatov.pigeoner.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import tech.filatov.pigeoner.model.flight.FlightType;
 
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class FlightDto {
@@ -18,4 +18,17 @@ public class FlightDto {
     private Integer myPassed;
     private LocalDateTime departure;
     private Integer passingThreshold;
+    private String flightType;
+
+    public FlightDto(Long id, String locationName, int distance, Long numberParticipants, Integer totalParticipants, Integer myPassed, LocalDateTime departure, Integer passingThreshold, FlightType flightType) {
+        this.id = id;
+        this.locationName = locationName;
+        this.distance = distance;
+        this.numberParticipants = numberParticipants;
+        this.totalParticipants = totalParticipants;
+        this.myPassed = myPassed;
+        this.departure = departure;
+        this.passingThreshold = passingThreshold;
+        this.flightType = flightType.getTitle();
+    }
 }
--- src/main/java/tech/filatov/pigeoner/repository/FlightRepository.java
+++ src/main/java/tech/filatov/pigeoner/repository/FlightRepository.java
@@ -10,21 +10,21 @@ import java.util.List;
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 
     @Query("SELECT new tech.filatov.pigeoner.dto.FlightDto(" +
-            "f.id, f.locationName, f.distance, count(fr), f.myPassed, f.totalParticipants, f.departure, f.passingThreshold) " +
+            "f.id, f.locationName, f.distance, count(fr), f.myPassed, f.totalParticipants, f.departure, f.passingThreshold, f.flightType) " +
             "FROM FlightResult fr " +
             "JOIN Pigeon p ON fr.pigeon.id = p.id " +
             "JOIN Flight f ON fr.flight.id = f.id " +
             "WHERE p.isNative = true AND fr.owner.id = :userId AND f.owner.id = :userId AND p.owner.id = :userId " +
-            "GROUP BY f.id, f.locationName, f.distance, f.myPassed, f.totalParticipants, f.id, f.departure, f.passingThreshold")
+            "GROUP BY f.id, f.locationName, f.distance, f.myPassed, f.totalParticipants, f.id, f.departure, f.passingThreshold, f.flightType")
     List<FlightDto> getAllFlightDto(long userId);
 
     @Query("SELECT new tech.filatov.pigeoner.dto.FlightDto(" +
-            "f.id, f.locationName, f.distance, count(fr), f.myPassed, f.totalParticipants, f.departure, f.passingThreshold) " +
+            "f.id, f.locationName, f.distance, count(fr), f.myPassed, f.totalParticipants, f.departure, f.passingThreshold, f.flightType) " +
             "FROM FlightResult fr " +
             "JOIN Pigeon p ON fr.pigeon.id = p.id " +
             "JOIN Flight f ON fr.flight.id = f.id " +
             "WHERE p.isNative = true AND f.id = :id AND fr.owner.id = :userId AND f.owner.id = :userId AND p.owner.id = :userId " +
-            "GROUP BY f.id, f.locationName, f.distance, f.myPassed, f.totalParticipants, f.id, f.departure, f.passingThreshold")
+            "GROUP BY f.id, f.locationName, f.distance, f.myPassed, f.totalParticipants, f.id, f.departure, f.passingThreshold, f.flightType")
     FlightDto getFlightDtoById(long id, long userId);
 
 }
