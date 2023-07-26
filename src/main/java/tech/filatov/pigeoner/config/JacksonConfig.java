7dc3e787bbb95e3ee131552aa6cb499ce9938965
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-07-26 13:21:13 -0300

    Add correct processing of empty filter params

---
+++ src/main/java/tech/filatov/pigeoner/config/JacksonConfig.java
@@ -0,0 +1,23 @@
+package tech.filatov.pigeoner.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class JacksonConfig {
+
+    private final ObjectMapper objectMapper;
+
+    public JacksonConfig(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @PostConstruct
+    public void objectMapperSetup() {
+        objectMapper.configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true);
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+    }
+}
