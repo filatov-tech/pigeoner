824949c793238c2410323b35a2f343a51f7595ad
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-08-14 20:29:51 -0300

    Refactor dto for sending errors info

--- src/main/java/tech/filatov/pigeoner/dto/ApiError.java
+++ src/main/java/tech/filatov/pigeoner/dto/ApiError.java
@@ -1,14 +1,19 @@
 package tech.filatov.pigeoner.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
-public class ErrorsListResponse {
+public class ApiError {
+    private String message;
+    private HttpStatus status;
     private List<ErrorInfo> errors;
 }
