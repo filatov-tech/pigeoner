032fbc11e6de63623c665b16e9a718b023ed4575
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-08-15 12:33:48 -0300

    Add IllegalRequestDataException.java

---
+++ src/main/java/tech/filatov/pigeoner/util/exception/IllegalRequestDataException.java
@@ -0,0 +1,7 @@
+package tech.filatov.pigeoner.util.exception;
+
+public class IllegalRequestDataException extends RuntimeException {
+    public IllegalRequestDataException(String message) {
+        super(message);
+    }
+}
