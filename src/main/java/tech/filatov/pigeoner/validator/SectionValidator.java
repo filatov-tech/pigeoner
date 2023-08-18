e3d7e5b08534fd5cb2161357abf39cbed78a306e
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-08-18 16:40:48 -0300

    Add validator for sections

---
+++ src/main/java/tech/filatov/pigeoner/validator/SectionValidator.java
@@ -0,0 +1,40 @@
+package tech.filatov.pigeoner.validator;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import tech.filatov.pigeoner.model.dovecote.Section;
+import tech.filatov.pigeoner.model.dovecote.SectionType;
+
+@Component
+public class SectionValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Section.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Section section = (Section) target;
+        Section parent = section.getParent();
+        SectionType parentType = parent == null ? null : parent.getType();
+
+        switch (section.getType()) {
+            case NEST -> {
+                if (parent == null || parentType == SectionType.NEST) {
+                    errors.rejectValue("parent", "", "Гнездо должно быть либо в голубятне, либо в одной из ее секций");
+                }
+            }
+            case ROOM -> {
+                if (parent == null || parentType == SectionType.NEST) {
+                    errors.rejectValue("parent", "", "Секция может быть либо частью голубятни, либо частью другой секции");
+                }
+            }
+            case DOVECOTE -> {
+                if (parent != null) {
+                    errors.rejectValue("parent", "", "Голубятня не может быть частью другой голубятни или секции");
+                }
+            }
+        }
+    }
+}
