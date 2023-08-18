d18cba6584118580ad4ac960cd12d14a9111ecc0
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-08-18 16:32:22 -0300

    Add check-new method to all entities and DTOs
    Added to reduce the amount of code and methods in the service layer

--- src/main/java/tech/filatov/pigeoner/HasId.java
+++ src/main/java/tech/filatov/pigeoner/HasId.java
@@ -1,6 +1,13 @@
 package tech.filatov.pigeoner;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public interface HasId {
     Long getId();
     void setId(Long id);
+
+    @JsonIgnore
+    default boolean isNew() {
+        return getId() == null;
+    }
 }
