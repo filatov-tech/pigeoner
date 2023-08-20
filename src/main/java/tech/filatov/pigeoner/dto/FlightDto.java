10017c465668d09fde3624f7ee1fddde8aceea18
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-08-20 10:42:16 -0300

    Modify flight constraints

--- src/main/java/tech/filatov/pigeoner/dto/FlightDto.java
+++ src/main/java/tech/filatov/pigeoner/dto/FlightDto.java
@@ -2,18 +2,25 @@ package tech.filatov.pigeoner.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 import tech.filatov.pigeoner.model.flight.FlightType;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
 public class FlightDto extends BaseDto {
+    @NotNull(message = "У точки вылета должны быть название и дистанция")
+    @NotEmpty(message = "У точки вылета должны быть название и дистанция")
     private String locationName;
+    @Range(min = 1, max = 2000, message = "Дистанция должна быть в диапозоне от 1 км до 2000 км")
     private int distance;
     private Long numberParticipants;
     private Integer totalParticipants;
     private Integer myPassed;
+    @NotNull
     private LocalDateTime departure;
     private Integer passingThreshold;
     private String flightType;
--- src/main/java/tech/filatov/pigeoner/model/flight/LaunchPoint.java
+++ src/main/java/tech/filatov/pigeoner/model/flight/LaunchPoint.java
@@ -2,11 +2,17 @@ package tech.filatov.pigeoner.model.flight;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Range;
 import tech.filatov.pigeoner.model.AbstractOwnedEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 
+@Table(uniqueConstraints = {
+        @UniqueConstraint(name = "UniqueNameWithDistance", columnNames = {"name", "distance"})
+})
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -15,5 +21,6 @@ public class LaunchPoint extends AbstractOwnedEntity {
     @NotNull
     private String name;
 
+    @Range(min = 1, max = 2000)
     private int distance;
 }
