14fad4e0f9fc08fbb830d52e06dc74596b78b293
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-08-22 16:06:12 -0300

    Modify FlightDto
    The data on the launch point is placed in a nested DTO. Added error messages and a couple of new constructors

--- src/main/java/tech/filatov/pigeoner/dto/FlightDto.java
+++ src/main/java/tech/filatov/pigeoner/dto/FlightDto.java
@@ -2,38 +2,56 @@ package tech.filatov.pigeoner.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Range;
+import lombok.Setter;
 import tech.filatov.pigeoner.model.flight.FlightType;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
+@Setter
 public class FlightDto extends BaseDto {
-    @NotNull(message = "У точки вылета должны быть название и дистанция")
-    @NotEmpty(message = "У точки вылета должны быть название и дистанция")
-    private String locationName;
-    @Range(min = 1, max = 2000, message = "Дистанция должна быть в диапозоне от 1 км до 2000 км")
-    private int distance;
+    @NotNull(message = "Вылет должен иметь информацию о точке запуска: место и расстояние")
+    private LaunchPointDto launchPoint;
     private Long numberParticipants;
     private Integer totalParticipants;
     private Integer myPassed;
-    @NotNull
+    @NotNull(message = "Вылет должен иметь информацию о дате и времени выпуска голубей")
     private LocalDateTime departure;
     private Integer passingThreshold;
+    @NotNull(message = "Вылет должен быть причислен к одному из типов")
     private String flightType;
 
     public FlightDto(Long id, String locationName, int distance, Long numberParticipants, Integer totalParticipants, Integer myPassed, LocalDateTime departure, Integer passingThreshold, FlightType flightType) {
         this.id = id;
-        this.locationName = locationName;
-        this.distance = distance;
+        this.launchPoint = new LaunchPointDto(locationName, distance);
         this.numberParticipants = numberParticipants;
         this.totalParticipants = totalParticipants;
         this.myPassed = myPassed;
         this.departure = departure;
         this.passingThreshold = passingThreshold;
         this.flightType = flightType.getTitle();
+    }
+
+    public FlightDto(Long id, Long launchPointId, String locationName, int distance, Integer totalParticipants, Integer myPassed, LocalDateTime departure, Integer passingThreshold, FlightType flightType) {
+        this.id = id;
+        this.launchPoint = new LaunchPointDto(launchPointId, locationName, distance);
+        this.totalParticipants = totalParticipants;
+        this.myPassed = myPassed;
+        this.departure = departure;
+        this.passingThreshold = passingThreshold;
+        this.flightType = flightType.getTitle();
+    }
+
+    public FlightDto(Long id, String locationName, int distance, Long numberParticipants, Integer totalParticipants, Integer myPassed, LocalDateTime departure, Integer passingThreshold, String flightType) {
+        this.id = id;
+        this.launchPoint = new LaunchPointDto(locationName, distance);
+        this.numberParticipants = numberParticipants;
+        this.totalParticipants = totalParticipants;
+        this.myPassed = myPassed;
+        this.departure = departure;
+        this.passingThreshold = passingThreshold;
+        this.flightType = flightType;
     }
 }
