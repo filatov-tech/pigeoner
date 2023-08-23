b065ae6add121e4aed12a38f7202aad36b8c19dc
filatov <58160752+filatov-dd@users.noreply.github.com>
2023-08-23 20:12:58 -0300

    Add FlightResultMapper

---
+++ src/main/java/tech/filatov/pigeoner/util/FlightResultMapper.java
@@ -0,0 +1,26 @@
+package tech.filatov.pigeoner.util;
+
+import org.springframework.stereotype.Component;
+import tech.filatov.pigeoner.dto.FlightResultDto;
+import tech.filatov.pigeoner.model.flight.AfterFlightCondition;
+import tech.filatov.pigeoner.model.flight.FlightResult;
+
+@Component
+public class FlightResultMapper {
+    public FlightResultMapper() {}
+
+    public FlightResult instantiateFrom(FlightResultDto dto) {
+        FlightResult flightResult = new FlightResult();
+        return setDataTo(flightResult, dto);
+    }
+
+    public FlightResult fillInstantiatedWith(FlightResultDto source, FlightResult target) {
+        return setDataTo(target, source);
+    }
+
+    private FlightResult setDataTo(FlightResult target, FlightResultDto source) {
+        target.setArrivalTime(source.getArrivalTime());
+        target.setCondition(AfterFlightCondition.valueOf(source.getAfterFlightCondition()));
+        return target;
+    }
+}
